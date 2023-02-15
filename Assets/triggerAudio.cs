6498e2314a6847ebc1bcd8ec99a12992155574ab
ThaisAlvarenga <ata381@nyu.edu>
2023-02-15 08:36:01 +0400

    Fixed audio sound levels (added audio mixer) and changed script so audio triggers once

--- Assets/triggerAudio.cs
+++ Assets/triggerAudio.cs
@@ -4,17 +4,29 @@ public class triggerAudio : MonoBehaviour
 {
     //public AudioClip myAudioClip;
     private AudioSource myAudioSource;
+
+    //variable to prevent audio from triggering more than once
+    bool canPlay;
+
     // Start is called before the first frame update
     void Start()
     {
         //get audio source from object
         myAudioSource = GetComponent<AudioSource>();
+
+        canPlay = true;
         // assign the clip we want to the audio source
         //myAudioSource.clip = myAudioClip;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        myAudioSource.Play();
+        if(canPlay)
+        {
+            myAudioSource.Play();
+            canPlay = false;
+
+        }
+        
     }
 }
