6bdf11d182c2aa2ebe97e264dfb18eac94c65bbf
Thais Alvarenga <ata381@nyu.edu>
2023-04-15 12:55:54 +0400

    Build for Oculus with animations in cinemachine track

---
+++ Assets/triggerAnimatoin.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerAnimatoin : MonoBehaviour
+{
+    public GameObject prevAnimation = null;
+    public GameObject newAnimation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        newAnimation.SetActive(false);
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        prevAnimation.SetActive(false);
+        newAnimation.SetActive(true);
+        Debug.Log("Animation should be displayed");
+    }
+
+}
